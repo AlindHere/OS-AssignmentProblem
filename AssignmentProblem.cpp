9531d2d94532a0f0534cecfbae0b2f160a41939e
AlindHere <49024467+AlindHere@users.noreply.github.com>
2019-03-31 14:14:06 +0530

    Add files via upload

---
+++ AssignmentProblem.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+void main()
+{
+int quespaper=1,paper=1,pen=1;
+int st1=fork();
+int st2=fork();
+int st2=fork();
+
+
+
+
+}
+
