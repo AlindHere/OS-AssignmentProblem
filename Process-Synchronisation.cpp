ecb93cd037bb720df813dc0e144419895afc9e7c
AlindHere <49024467+AlindHere@users.noreply.github.com>
2019-04-02 22:25:02 +0530

    Add files via upload

---
+++ Process-Synchronisation.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<semaphore.h>
+#include<pthread.h>
+int pen=1,quespaper=1,paper=1;
+sem_t s;
+
+
+void main()
+{
+
+sem_init(&s,0,3);
+
+}
